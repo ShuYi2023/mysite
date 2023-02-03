7ae9e98e36a7cfc0d9ec05d413de6df727043724
ShuYi2023 <shu.yi2023@outlook.com>
2023-02-03 16:38:53 +0800

    migrated01

--- blog/models.py
+++ blog/models.py
@@ -10,10 +10,10 @@ STATUS = (
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    subject = models.CharField(max_length=200, unique=True)
-    project = models.CharField(max_length=200, unique=True)
-    chapter = models.CharField(max_length=200, unique=True)
-    level = models.CharField(max_length=200, unique=True)
+    section = models.CharField(max_length=200)
+    project = models.CharField(max_length=200)
+    chapter = models.CharField(max_length=200)
+    level = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
