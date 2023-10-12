390a51784c6cdc338d158a8b37b36225c165a2ab
maxisbest <maxyi2002@hotmail.com>
2023-10-12 20:54:57 +0800

    debug = false

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-0yz^v%3s&kt4qdj#zb7335(jb@dnoo4q9srf+r%lsq0b&6w6la'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
 
